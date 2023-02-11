474fc262568f902338d49bb36c26696e3cc09191
BrianKheng <briankheng63@gmail.com>
2023-02-12 00:06:20 +0700

    add: teleporter class

---
+++ src/main/java/Models/Teleporter.java
@@ -0,0 +1,59 @@
+package Models;
+
+import java.util.UUID;
+
+public class Teleporter {
+    private UUID id;
+    private boolean isTeleporterActive;
+    private int heading;
+    private Position targetPos;
+    public int cntNotFound;
+
+    public Teleporter(){
+        id = null;
+        isTeleporterActive = false;
+        heading = -1;
+        targetPos = new Position();
+        cntNotFound = 0;
+    }
+
+    public void setTeleporterActive(boolean value){
+        this.isTeleporterActive = value;
+    }
+
+    public boolean isTeleporterActive(){
+        return isTeleporterActive;
+    }
+
+    public void setId(UUID id){
+        this.id = id;
+    }
+
+    public UUID getId(){
+        return id;
+    }
+
+    public void setHeading(int heading){
+        this.heading = heading;
+    }
+
+    public int getHeading(){
+        return heading;
+    }
+
+    public void setTargetPosition(Position targetPos){
+        this.targetPos = targetPos;
+    }
+
+    public Position getTargetPosition(){
+        return targetPos;
+    }
+
+    public void reset(){
+        id = null;
+        isTeleporterActive = false;
+        heading = -1;
+        targetPos = new Position();
+        cntNotFound = 0;
+    }
+}
